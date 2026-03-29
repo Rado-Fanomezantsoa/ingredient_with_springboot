eaeae5d69cf1c61c1b2ab0fb2ce0a0f5765a6fcb
Rado <nomenaratovonjanahary@gmail.com>
2026-03-29 11:51:06 +0300

    first commit

---
+++ src/main/java/org/ingredient/ingredientspringboot/IngredientSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.ingredient.ingredientspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngredientSpringBootApplication.class, args);
+    }
+
+}
